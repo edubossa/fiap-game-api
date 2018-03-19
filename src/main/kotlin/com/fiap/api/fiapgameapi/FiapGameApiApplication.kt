8b6a8151becd964e4e895bdcee6e91cfc35512a4
edubossa <edubossa@gmail.com>
2018-03-19 11:18:51 -0300

    adicionando urls de acesso

--- src/main/kotlin/com/fiap/api/fiapgameapi/FiapGameApiApplication.kt
+++ src/main/kotlin/com/fiap/api/fiapgameapi/FiapGameApiApplication.kt
@@ -3,6 +3,11 @@ package com.fiap.api.fiapgameapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/*
+    https://dashboard.heroku.com/apps/fiap-game-api/settings
+    https://fiap-game-api.herokuapp.com/games
+    https://mlab.com/databases/fiap-game/collections/game
+ */
 @SpringBootApplication
 class FiapGameApiApplication
 
